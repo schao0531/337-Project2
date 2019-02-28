908d472669cec6f4aedd8437169282ca50eeeb00
Nico (School Computer) <ntyjeski@gmail.com>
2019-02-28 17:39:22 -0600

    created pipeline for url to an ingredient dict
    just wrapped up working with Stacey and Chris. Format for ingredients looks the way it does in the JSON examples

--- Team23/recipe_api.py
+++ Team23/recipe_api.py
@@ -1,9 +1,9 @@
 '''Version 0.1'''
 from bs4 import BeautifulSoup
-#from urllib.request import Request, urlopen
 from requests import get
 
 #https://thewoksoflife.com/2018/08/peach-daiquiris-frozen/
+
 def autograder(url):
     '''Accepts the URL for a recipe, and returns a dictionary of the
     parsed results in the correct format. See project sheet for
@@ -13,8 +13,6 @@ def autograder(url):
 #    r = urlopen(req).read()
 #    soup = BeautifulSoup(r, "html.parser")
 #    print(soup.prettify())
-
-    grab_ingredients(url)
     #return results
 
 def get_raw_html(url):
@@ -29,6 +27,11 @@ def get_raw_html(url):
     except:
         print("URL ", url, " not recognized!")
 
+
+####################################################################################################################################
+# INGREDIENT STUFF
+####################################################################################################################################
+
 def grab_ingredients(url):
     html = get_raw_html(url)
     items = []
@@ -40,6 +43,50 @@ def grab_ingredients(url):
             items.append(segments[3])
     return items[:-1]
 
+def ingredient_parser(string):
+    words = string.split(' ')
+    
+    # Quantity
+    quantity = [words[0]]
+    words = words[1:]
+    
+    # Measurement
+    if '(' in words[0]:
+        measurement = ' '.join(words[:3])
+        measurement = measurement.replace("(","").replace(")","")
+        measurement = [measurement]
+        words = words[3:]
+    else:
+        measurement = [words[0]]
+        words = words[1:]
+
+    name = [' '.join(words)]
+
+    descriptor = []
+    preparation = []
+    prep_description = []
+    Max = []
+
+    return {"name":name,
+            "quantity":quantity,
+            "measurement":measurement,
+            "descriptor":descriptor,
+            "preparation":preparation,
+            "prep_description":prep_description,
+            "max":Max
+            }
+
+def ingredient_dict(string_list):
+    ingredient_list = []
+    for ingredient in string_list:
+        ingredient_list.append(ingredient_parser(ingredient))
+    return ingredient_list
+
+
+####################################################################################################################################
+# PREP STUFF
+####################################################################################################################################
+
 def grab_steps(url):
     html = get_raw_html(url)
     items = []
@@ -50,6 +97,7 @@ def grab_steps(url):
             segments = line.split('>')
             items.append(segments[1].split('\n')[0])
     return items[:-1]
+
 
 def main():
     url = str(input("What recipe would you like to read?: ")).strip()
