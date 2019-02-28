42dd33a59c9edb3c19b122a9d8e2346d4f5025e4
Nico (School Computer) <ntyjeski@gmail.com>
2019-02-28 16:31:21 -0600

    added new read and parse ingredients functions

--- Team23/recipe_api.py
+++ Team23/recipe_api.py
@@ -1,6 +1,7 @@
 '''Version 0.1'''
 from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
+#from urllib.request import Request, urlopen
+from requests import get
 
 #https://thewoksoflife.com/2018/08/peach-daiquiris-frozen/
 def autograder(url):
@@ -8,11 +9,31 @@ def autograder(url):
     parsed results in the correct format. See project sheet for
     details on correct format.'''
     # your code here
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    r = urlopen(req).read()
-    soup = BeautifulSoup(r, "html.parser")
-    print(soup.prettify())
+#    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+#    r = urlopen(req).read()
+#    soup = BeautifulSoup(r, "html.parser")
+#    print(soup.prettify())
+
+    grab_ingredients(url)
     #return results
+
+
+def get_raw_html(url):
+    x = get(url)
+    if x.status_code == 200:
+        return x.content
+    else:
+        return x.status_code
+
+def grab_ingredients(url):
+    html = get_raw_html(url)
+    items = []
+    for line in html.select('label'):
+        line = str(line)
+        if "{true: 'checkList__item'}" in line:
+            segments = line.split('"')
+            items.append(segments[3])
+    return items[:-1]
 
 def main():
     url = str(input("What recipe would you like to read?: ")).strip()
