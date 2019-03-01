696564c2d30a014a1798135a11c1917b91d58ed5
schao0531 <staceychao2019@u.northwestern.edu>
2019-02-28 23:09:16 -0600

    stacey c implemented tool parser

--- Team23/recipe_api.py
+++ Team23/recipe_api.py
@@ -1,9 +1,13 @@
 '''Version 0.1'''
 from bs4 import BeautifulSoup
 from requests import get
+import re
+import json
+global results
+from pprint import pprint
+import pdb
 
-#https://thewoksoflife.com/2018/08/peach-daiquiris-frozen/
-
+#https://www.allrecipes.com/recipe/80827/easy-garlic-broiled-chicken/
 def autograder(url):
     '''Accepts the URL for a recipe, and returns a dictionary of the
     parsed results in the correct format. See project sheet for
@@ -13,6 +17,9 @@ def autograder(url):
 #    r = urlopen(req).read()
 #    soup = BeautifulSoup(r, "html.parser")
 #    print(soup.prettify())
+
+    #grab_ingredients(url)
+    grab_steps(url)
     #return results
 
 def get_raw_html(url):
@@ -34,6 +41,7 @@ def get_raw_html(url):
 
 def grab_ingredients(url):
     html = get_raw_html(url)
+    soup = BeautifulSoup(html, "html.parser")
     items = []
     for line in html.select('label'):
         #         print(line)
@@ -41,15 +49,16 @@ def grab_ingredients(url):
         if "{true: 'checkList__item'}" in line:
             segments = line.split('"')
             items.append(segments[3])
+    print(items)
     return items[:-1]
 
 def ingredient_parser(string):
     words = string.split(' ')
-    
+
     # Quantity
     quantity = [words[0]]
     words = words[1:]
-    
+
     # Measurement
     if '(' in words[0]:
         measurement = ' '.join(words[:3])
@@ -89,18 +98,42 @@ def ingredient_dict(string_list):
 
 def grab_steps(url):
     html = get_raw_html(url)
-    items = []
+    steps = []
     for line in html.select('span'):
         #         print(line)
         line = str(line)
         if "recipe-directions__list--item" in line:
             segments = line.split('>')
-            items.append(segments[1].split('\n')[0])
-    return items[:-1]
+            steps.append(segments[1].split('\n')[0])
+    get_tools(steps[:-1])
+    return steps[:-1]
 
+def get_tools(steps):
+    cooking_tools = []
+    implied_tools = []
+    official_tools = {}
+    with open('tools.json') as f:
+        official_tools = json.load(f)
+
+    for s in steps:
+        line = s.lower().strip()
+        line = re.sub(r'[^\w\s]','',line)
+        print(line)
+        for t in official_tools:
+            if t in line:
+                cooking_tools.append(t)
+            elif official_tools[t]:
+                for w in official_tools[t]:
+                    if w in line:
+                        implied_tools.append(t)
+
+    print(list(set(cooking_tools)))
+    print(list(set(implied_tools)))
+    return list(set(cooking_tools)), list(set(implied_tools))
 
 def main():
-    url = str(input("What recipe would you like to read?: ")).strip()
+    #url = str(input("What recipe would you like to read?: ")).strip()
+    url = "http://allrecipes.com/Recipe/Spaghetti-Carbonara-II/"
     autograder(url)
 
 if __name__ == '__main__':
